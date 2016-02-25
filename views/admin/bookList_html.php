bf36be8b6c2d008d5fc2fe40e38c6683ce418d77
Yves Talboom <yves.talboom@gmail.com>
2016-02-25 15:59:34 +0100

    format code

--- views/admin/bookList_html.php
+++ views/admin/bookList_html.php
@@ -13,7 +13,7 @@ while ($book = $books->fetchObject()) {
     $bookDetailLink = "index.php?page=bookDetail&amp;bookId=$book->id";
     $response .= " <tr>";
     $response .= "<td>" . $book->id . "</td>";
-    $response .= "<td><a href='".$bookDetailLink."'>" . $book->title . "</a></td>";
+    $response .= "<td><a href='" . $bookDetailLink . "'>" . $book->title . "</a></td>";
     $response .= " </tr>";
 }
 $response .= "</tbody>";

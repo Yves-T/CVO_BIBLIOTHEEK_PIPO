4a249f79e2398b69c5f5a6ed0baab821500529f1
Yves Talboom <yves.talboom@gmail.com>
2016-02-25 16:14:38 +0100

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bibliotheek 404 foutmelding</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oeps!</h1>
+                <h2>
+                    404 Foutmelding</h2>
+                <div class="error-details">
+                    Sorry, maar er is een fout opgetreden, de gevraagde pagina is niet gevonden!
+                </div>
+                <div class="error-actions">
+                    <a href="index.php" class="btn btn-primary btn-lg">
+                        <span class="glyphicon glyphicon-home"></span>
+                        Neem me terug naar de Home pagina </a>
+                    <a href="mailto:info@cvodeverdieping.be" target="_blank" class="btn btn-default btn-lg">
+                        <span class="glyphicon glyphicon-envelope"></span> Contacteer Support
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+

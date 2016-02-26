94191cde8a22ab2135577843b7fefaf953aabdf1
Yves Talboom <yves.talboom@gmail.com>
2016-02-26 09:43:12 +0100

    Add target blank to footer link

--- views/admin/footer.php
+++ views/admin/footer.php
@@ -5,7 +5,8 @@ return "
 <div class=\"col-sm-12 col-lg-12\">
 <div id=\"footer\">
     <div class=\"container\">
-        <p class=\"text-muted credit\">Copyright &copy; <a href=\"http://www.cvodeverdieping.be/\">CVO De Verdieping</a>
+        <p class=\"text-muted credit\">Copyright &copy;
+        <a href=\"http://www.cvodeverdieping.be/\" target='_blank'>CVO De Verdieping</a>
             Schachtplein 1 3550 Heusden-zolder
     </div>
 </div>

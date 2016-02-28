18463ee87835774e9d98f24b3c4f21f103aa7681
Yves Talboom <yves.talboom@gmail.com>
2016-02-28 08:45:03 +0100

    Update navbar brand information

--- views/admin/adminNavigation.php
+++ views/admin/adminNavigation.php
@@ -18,7 +18,7 @@ $response = "
         <span class=\"icon-bar\"></span>
         <span class=\"icon-bar\"></span>
         </button>
-        <a class=\"navbar-brand\" href=\"#\">Bibliotheek</a>
+        <a class=\"navbar-brand\" href=\"index.php\">Bibliotheek pipo</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
